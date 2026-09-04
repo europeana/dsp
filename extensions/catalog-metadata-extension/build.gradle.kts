6b882f1bd93eee913cec261d3ed3ab5be9253b09
srishtisingh <srishti.singh@europeana.eu>
2026-09-04 12:20:52 +0200

    EA-4623: remove commented code

--- extensions/catalog-metadata-extension/build.gradle.kts
+++ extensions/catalog-metadata-extension/build.gradle.kts
@@ -23,14 +23,3 @@ dependencies {
     api(libs.edc.ext.jsonld)
     api(libs.edc.controlplane.catalog)
 }
-
-
-//dependencies {
-//    testImplementation(platform("org.junit:junit-bom:6.0.0"))
-//    testImplementation("org.junit.jupiter:junit-jupiter")
-//    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-//}
-//
-//tasks.test {
-//    useJUnitPlatform()
-//}
